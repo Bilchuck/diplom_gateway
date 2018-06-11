7fb0412e6062863e9bff473b11b210c0cac5f3af
Bilchuck <anton.bilchuk@stylelounge.com>
2018-06-12 01:35:16 +0300

    return back to root pass

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('temperature', 'client', '', {
+const sequelize = new Sequelize('temperature', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
   multipleStatements: true,
