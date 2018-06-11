cbe0f0e404239d2f5a01859cfda97d62b02ae72f
Bilchuck <anton.bilchuk@stylelounge.com>
2018-06-12 00:53:44 +0300

    feature: add init.sql file

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `records` IF NOT EXISTS (
+  `id` char(36) NOT NULL,
+  `temperature` int(11) NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
