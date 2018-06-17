ed6430a32b0adbd9e5761752651176a4db7b5694
Bilchuck <anton.bilchuk@stylelounge.com>
2018-06-17 22:42:01 +0300

    fix: fix scheduler

--- src/records.js
+++ src/records.js
@@ -4,9 +4,22 @@ const util = require('util')
 
 const readFile = util.promisify(fs.readFile)
 const appendFile = util.promisify(fs.appendFile)
+const exists = util.promisify(fs.exists)
+const writeFile = util.promisify(fs.writeFile)
 const filePath = path.join(__dirname, 'records.txt')
 
-const appendData = content => appendFile(filePath, `${content}\n`, 'utf8')
+const appendData = async content => {
+  const data = await readFile(filePath, 'utf8')
+  const array = data.split('\n')
+  const start = array.slice(-4)
+
+  const newFile = [...start, content].join('\n')
+  await saveDb(newFile)
+}
+
+const saveDb = async content => {
+  await writeFile(filePath, content, 'utf8')
+}
 
 const getData = async () => {
   const data = await readFile(filePath, 'utf8')
@@ -20,7 +33,19 @@ const getData = async () => {
   return chart
 }
 
+const createIfNotExistsDb = async () => {
+  const fileExists = await exists(filePath)
+  if (!fileExists) {
+    try {
+      await saveDb('')
+    } catch (e) {
+      consolel.log(e)
+    }
+  }
+}
+
 module.exports = {
   getData,
-  appendData
+  appendData,
+  createIfNotExistsDb
 }
--- src/scheduler.js
+++ src/scheduler.js
@@ -1,12 +1,14 @@
 const schedule = require('node-schedule')
 const { getTemperature } = require('./services/temperature')
-const { appendData } = require('./records')
+const { appendData, createIfNotExistsDb } = require('./records')
 
 const scheduler = () => {
-  schedule.scheduleJob('*/5 * * * * *', async () => {
-    const temperature = await getTemperature()
-    const time = new Date().toISOString()
-    await appendData(`${temperature} | ${time}`)
+  createIfNotExistsDb().then(() => {
+    schedule.scheduleJob('*/5 * * * * *', async () => {
+      const temperature = await getTemperature()
+      const time = new Date().toISOString()
+      await appendData(`${temperature} | ${time}`)
+    })
   })
 }
 
